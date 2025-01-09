02d89c0f11b5c7c5488fac8059b407a5f73eca67
dokens <robinlefevre85@gmail.com>
2025-01-09 18:20:53 +0100

    Fixed the active mole to false when whacked

--- src/components/Mole.tsx
+++ src/components/Mole.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 import '../styles/Mole.sass';
 import { MoleProps } from "../typescript/MoleType";
+import { useDispatch } from 'react-redux';
+import { setActiveMole } from '../redux/features/gameSlice';
 
 export default function Mole({id,isActive}: MoleProps) {
 
-    const [isWhacked, setIsWhacked] = useState(false);
+    // const [isWhacked, setIsWhacked] = useState(false);
+    const dispatch = useDispatch();
 
     const handleWhack = () => {
         if(isActive){
             console.log(`Mole ${id} whacked!`);
-            setIsWhacked(true);
+            // setIsWhacked(true);
+            dispatch(setActiveMole(null));
         }
     }
-
+    
     return (
         <>
             <div className={isActive ? "mole" : "hole"} onClick={handleWhack}></div>
