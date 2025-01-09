74600b73ed00d8f3be33a71cd1e60bf2a2c7e2c2
dokens <robinlefevre85@gmail.com>
2025-01-09 15:47:06 +0100

    Added game logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,14 +1,24 @@
 import '../styles/Game.sass'
+import { useEffect, useState } from 'react';
 
 export default function Game() {
 
-    
+    const [activeMole, setActiveMole] = useState<number | null>(null);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const randomMole = Math.floor(Math.random() * 12);
+            setActiveMole(randomMole);
+        }, 1000)
+
+        return () => clearInterval(interval);
+    }, []);
 
     return(
         <>
             <div className='game'>
                 <div className="board">
-                    {Array.from({length: 12}, (_, i) => <div key={i} className="hole" style={{backgroundColor : 'red'}}></div>)}
+                    {Array.from({length: 12}, (_, i) => <div key={i} className="hole" style={activeMole === i ? { backgroundColor: 'green' } : { backgroundColor: 'red'}}></div>)}
                 </div>
             </div>
         </>
