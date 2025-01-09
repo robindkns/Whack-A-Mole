e60c117f855a13e0015bf893248779c56bebd635
dokens <robinlefevre85@gmail.com>
2025-01-09 15:29:58 +0100

    Added Game Grid

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,15 @@
 import '../styles/Game.sass'
 
 export default function Game() {
+
+    
+
     return(
         <>
             <div className='game'>
-                <h1>Game</h1>
+                <div className="board">
+                    {Array.from({length: 12}, (_, i) => <div key={i} className="hole" style={{backgroundColor : 'red'}}></div>)}
+                </div>
             </div>
         </>
     )
