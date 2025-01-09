d1f29008d771c5a5a4dbf626118d691fac477298
dokens <robinlefevre85@gmail.com>
2025-01-09 20:26:35 +0100

    Added function to format Time

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,10 +3,17 @@ import Mole from "./Mole";
 import { BoardProps } from "../typescript/BoardProps";
 
 export default function Board( {activeMole,score,timeLeft}:BoardProps ) {
+    
+    function formatTime(timeLeft: number) {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+    
     return (
         <>
             <h1 className="score">Score : {score}</h1>
-            <h1 className="timer">Time Left : {timeLeft}</h1>
+            <h1 className="timer">Time Left : {formatTime(timeLeft)}</h1>
             <div className="board">
                 {Array.from({ length: 12 }, (_, index) => (
                     <Mole key={index} id={index} isActive={activeMole === index} />
