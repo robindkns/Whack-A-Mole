d713f1231f951b1bc545a4695c7549068f891ad1
dokens <robinlefevre85@gmail.com>
2025-01-11 16:18:46 +0100

    Created OldMonitor component and style sheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import './App.sass';
+import './styles/App.sass';
 
 import Game from './components/Game';
+import OldMonitor from './components/OldMonitor';
 
 function App() {
   return (
     <>
-      <div id='border1'></div>
-      <div id='border2'></div>
-      <div id="border3"></div>
-      <div id='border4'></div>
+      <OldMonitor />
       <Game />
     </>
   );
---
+++ src/components/OldMonitor.tsx
@@ -0,0 +1,12 @@
+import '../styles/OldMonitor.sass';
+
+export default function OldMonitor() {
+    return (
+        <>
+            <div id="border1"></div>
+            <div id="border2"></div>
+            <div id="border3"></div>
+            <div id="border4"></div>
+        </>
+    );
+}
