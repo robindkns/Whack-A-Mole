8b7d0a73170aeb28edfe914b0815cdad1e999d4c
dokens <robinlefevre85@gmail.com>
2025-01-11 16:15:57 +0100

    Added Old Monitor Screen Border style around the game

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import Game from './components/Game';
 function App() {
   return (
     <>
+      <div id='border1'></div>
+      <div id='border2'></div>
+      <div id="border3"></div>
+      <div id='border4'></div>
       <Game />
     </>
   );
